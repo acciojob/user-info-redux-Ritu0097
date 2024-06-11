ad63c36036f692a6e30a8161334c2bd3c5cb5bbb
Ritu0097 <rituchaudhary0097@gmail.com>
2024-06-11 09:46:06 +0530

    form

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { nameValue, mailValue } from '../redux/action/action.creator'
+import { useDispatch,useSelector } from "react-redux";
+import { changeName,changeEmail } from "./redux/actions/changeActions";
 
 const App = () => {
-  let { userName, mail } = useSelector(state => state);
-  const dispatch = useDispatch();
-
+  let x= useSelector((data)=>data)
+  let dispatch=useDispatch()
   return (
     <div>
-      <div>
-        <h1>User Information</h1>
+      <h1>User Information</h1>
         <label>Name:</label>
-        <input type="text" onChange={(e) => { dispatch(nameValue(e.target.value)) }}></input>
-        <br></br>
+        <input type='text' onChange={(event)=>dispatch(changeName(event.target.value))}></input><br></br>
         <label>Email:</label>
-        <input type="email" onChange={(e) => { dispatch(mailValue(e.target.value)) }}></input>
-      </div>
-      <p>Current values in store:</p>
-      <div className="output">
-        Name - {userName}
-        Email - {mail}
-      </div>
+        <input type='email' onChange={(event)=>dispatch(changeEmail(event.target.value))}></input>
+        <p>Current Values in store</p>
+        <p className="output">Name - {x.name}</p>
+        <p className="output">Email - {x.email}</p>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store } from "./components/redux/store";
 
 
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
